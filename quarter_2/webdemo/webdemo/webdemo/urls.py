8a7ccb391e976dfcde809e1a4bbcec61432fc2e0
Michal Gorlas <micgor32@fsfe.org>
2024-12-17 19:42:06 +0100

    fix: removed admin access for the django app

--- quarter_2/webdemo/webdemo/webdemo/urls.py
+++ quarter_2/webdemo/webdemo/webdemo/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.urls import path
 from webdemo.scanner.views import index
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path('', index, name='index'),
 ]
